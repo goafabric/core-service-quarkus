faa99e812c7173f199a3384a4a7348210720e639
Andreas Mautsch <amautsch@gmx.de>
2026-08-27 08:48:36 +0000

    Update dependency io.quarkiverse.mcp:quarkus-mcp-server-http to v1.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		implementation("io.quarkiverse.azureservices:quarkus-azure-storage-blob:1.2.5")
-		implementation("io.quarkiverse.mcp:quarkus-mcp-server-http:1.13.1")
+		implementation("io.quarkiverse.mcp:quarkus-mcp-server-http:1.13.2")
 
 		kapt("org.mapstruct:mapstruct-processor:1.6.3")
 
